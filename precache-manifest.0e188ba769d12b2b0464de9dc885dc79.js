b5f05995cbac9c08595979950fefeea422861b96
Milos <milosrancic94@gmail.com>
2019-03-05 18:50:59 +0100

    Updates

---
+++ precache-manifest.0e188ba769d12b2b0464de9dc885dc79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1bc0451b91dbacb2712",
+    "url": "/reactjs-calculator.git/static/css/main.44733c06.chunk.css"
+  },
+  {
+    "revision": "f1bc0451b91dbacb2712",
+    "url": "/reactjs-calculator.git/static/js/main.f1bc0451.chunk.js"
+  },
+  {
+    "revision": "71cf9f72f5152ce15b1e",
+    "url": "/reactjs-calculator.git/static/js/runtime~main.71cf9f72.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/reactjs-calculator.git/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "bf6165a427437990aeca05a54618a909",
+    "url": "/reactjs-calculator.git/index.html"
+  }
+];
